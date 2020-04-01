465fdde1e3b9d4fdd63ee7443d4c0eed87daeebd
Sreedeepthinukala <51539128+Sreedeepthinukala@users.noreply.github.com>
2020-03-31 20:22:41 -0400

    Add files via upload

---
+++ job.sh
@@ -0,0 +1,15 @@
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate tensorflow114 #activating the virtual environment to run the retraining model
+#automation.py will create cvat tasks automatically when run. If tasks are created, one you annotate and run this, 
+# it will be downloaded in xml format
+python /home/sree/Documents/TrainYourOwnYOLO_master_TESTING/1_Image_Annotation/cvat_automation.py 
+# This will convert all the xmls to csv
+python /home/sree/Documents/TrainYourOwnYOLO_master_TESTING/1_Image_Annotation/xml_to_csv.py
+# This will convert cvs to yolo format needed for retraining
+python /home/sree/Documents/TrainYourOwnYOLO_master_TESTING/1_Image_Annotation/Convert_to_YOLO_format.py
+# If you are running this model for the first time, you will need to download the weights.
+# If you have already ran it and downloaded the weights, you don't have to do it again. You can comment the below step
+python /home/sree/Documents/TrainYourOwnYOLO_master_TESTING/2_Training/Download_and_Convert_YOLO_weights.py
+# This is the retraining model.
+python /home/sree/Documents/TrainYourOwnYOLO_master_TESTING/2_Training/Train_YOLO.py
+# ./job.sh to run this .sh file.
